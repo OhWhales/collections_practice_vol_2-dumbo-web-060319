bbec8fc7a2b245f2ba29aaa481b6ced1ee6692c2
impartial-flow-3347 <brandon.lau86@gmail.com>
2019-06-04 12:57:20 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,15 @@
 # your code goes here
+
+def begins_with_r(arr)
+  bool x = true 
+  
+  counter = 0 
+  while (counter < arr.size) 
+    z=arr[counter]
+    if z[0] != "r"
+      x = false 
+    end 
+    return x 
+    
+  end 
+end 
